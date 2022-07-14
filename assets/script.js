e667c2f721b74c3ed3ee1db2e568c52c4307ec47
Keli <krsparks2@yahoo.com>
2022-07-14 14:37:19 -0700

    formatting changes

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+console.log(today.format("MMM Do, YYYY"))
+
+// on click funtion
+// when clicked writes to local storage 
+
+// function that retrieves the info from local storage 
+
+//below shows the correct color on the lines for past, present and future
+$("textarea").each(function(){
+    var textAreaParent = $(this).parent().siblings().attr("id");
+    // console.log(textAreaParent);
+    // console.log("hello")
+    var currentHour = moment().hour();
+    // console.log(currentHour);
+    if (textAreaParent > currentHour){
+        $(this).addClass("future");
+    }
+    else if (textAreaParent < currentHour){
+        $(this).addClass("past");
+    }
+    else {
+        $(this).addClass("present");
+    }
+})
